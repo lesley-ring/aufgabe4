aba457790a67f5a49a104877e4ec0e96cbd83960
clrokr <ikosa.de@gmail.com>
2013-07-08 10:04:22 +0200

    Client hinzugefügt

---
+++ M4/uni/prakinf/m4/client/forms/LobbyWindow.java
@@ -0,0 +1,85 @@
+package uni.prakinf.m4.client.forms;
+
+import uni.prakinf.m4.client.Client;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Vector;
+
+public class LobbyWindow extends JFrame {
+    private Client client;
+
+    private JTextPane messagePane;
+    private JPanel panel1;
+    private JTextField messageTextField;
+    private JButton mitspielenButton;
+    private JButton privateNachrichtButton;
+    private JButton neuesSpielButton;
+    private JList playerList;
+    private JButton abmeldenButton;
+
+    public LobbyWindow(Client client_) {
+        this.client = client_;
+
+        setContentPane(panel1);
+        setTitle("M4 Lobby");
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                client.onLobbyWindowExit();
+            }
+        });
+        messageTextField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                client.onMessageLineEnter(messageTextField.getText());
+                messageTextField.setText("");
+            }
+        });
+        neuesSpielButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                client.onNewGame();
+            }
+        });
+        mitspielenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (playerList.getSelectedValue() instanceof String) {
+                    String s = (String) playerList.getSelectedValue();
+                    if (!s.isEmpty())
+                        client.onJoinGame(playerList.getSelectedIndex());
+                }
+            }
+        });
+        abmeldenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                client.onLobbyWindowLogout();
+            }
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    public void resetState() {
+        playerList.setListData(new Vector());
+
+        messagePane.setText("Hallo!\n");
+        messageTextField.setText("");
+        messageTextField.requestFocus();
+    }
+
+    public void addLineToMessagePane(String message) {
+        messagePane.setText(messagePane.getText() + message + "\n");
+        messagePane.setCaretPosition(messagePane.getText().length() - 1);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void updatePlayerList(String[] list) {
+        playerList.setListData(list);
+    }
+}
---
+++ M4/uni/prakinf/m4/client/forms/LoginDialog.java
@@ -0,0 +1,53 @@
+package uni.prakinf.m4.client.forms;
+
+import uni.prakinf.m4.client.Client;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class LoginDialog extends JFrame {
+    private Client client;
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTextField serverTextField;
+    private JTextField benutzernameTextField;
+    private JPasswordField passwortPasswordField;
+
+    public LoginDialog(Client client_) {
+        this.client = client_;
+        setContentPane(contentPane);
+        getRootPane().setDefaultButton(buttonOK);
+        setTitle("M4 Login");
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                client.onLoginDialogExit();
+            }
+        });
+
+        buttonOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                client.onLoginDialogOKClick(serverTextField.getText(), benutzernameTextField.getText(), String.valueOf(passwortPasswordField.getPassword()));
+            }
+        });
+        buttonCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                client.onLoginDialogExit();
+            }
+        });
+    }
+
+    public void resetEntries() {
+        serverTextField.setText("localhost");
+        benutzernameTextField.setText("fghj");
+        passwortPasswordField.setText("fjgh");
+        invalidate();
+    }
+}
